ed8adde4fedd0794e29059b44447eff5d395ea1d
Vince <mvincem@gmail.com>
2022-12-04 17:10:30 +0100

    add prettier and eslint

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'flowbite';
-
+import 'flowbite'
 
 createApp(App).mount('#app')
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ]
+  plugins: [require('flowbite/plugin')],
 }
